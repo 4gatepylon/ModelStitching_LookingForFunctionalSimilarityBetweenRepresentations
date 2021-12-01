75c2c0fddc1a042f0a9de10df2b72aa24b2dd0aa
Adriano Hernandez <adrianoh@mit.edul>
2021-11-30 23:18:41 -0500

    real fix

--- mnist_long/main.py
+++ mnist_long/main.py
@@ -355,7 +355,7 @@ def test(model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(latent)
+            output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
