d03da0fe0cdd98b21e12a9b23dae9d87d40e7d05
Adriano Hernandez <adrianoh@mit.edul>
2022-02-16 12:28:30 -0500

    resnet now has a list of layers so that we can easily get intermediate inputs/outputs

--- long/resnet.py
+++ long/resnet.py
@@ -1,5 +1,5 @@
 ###
-### NOTE this copied from stitching-public
+### NOTE this copied from stitching-public with small modifications
 ###
 
 import torch.nn as nn
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 ## ResNet18 for CIFAR
 ## Based on: https://github.com/kuangliu/pytorch-cifar/blob/master/models/preact_resnet.py
 class PreActBlock(nn.Module):
-    '''Pre-activation version of the BasicBlock.'''
+    # Pre-activation version of the BasicBlock.
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1, **kwargs):
@@ -38,6 +38,12 @@ class PreActBlock(nn.Module):
         out += shortcut
         return out
 
+class View(nn.Module):
+    # Expects a function that given an x will return the shape to view it as
+    def __init__(self, viewshape):
+        self.viewshape = viewshape
+    def forward(self, x):
+        return x.view(*list(self.viewshape(x)))
 
 class PreActResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10, init_channels=64):
@@ -45,13 +51,24 @@ class PreActResNet(nn.Module):
         self.in_planes = init_channels
         c = init_channels
 
-        self.conv1 = nn.Conv2d(3, c, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        # We begin with a convolutional layer to get the right shape
+        self.conv1 = nn.Conv2d(3, c, kernel_size=3, stride=1, padding=1, bias=False)
+
+        # Then we have 4 residual-style block layers
         self.layer1 = self._make_layer(block, c, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 2*c, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 4*c, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 8*c, num_blocks[3], stride=2)
+
+        # Classifier will be last layer
+        self.avg_pool = nn.AvgPool2d(4)
+        self.flattener = View(lambda out: (out.size(0), -1))
         self.linear = nn.Linear(8*c*block.expansion, num_classes)
+
+        self.classifier = nn.Sequential(self.avg_pool, self.flattener, self.linear)
+
+        # This sequence makes it easy to insert intermediate inputs and take out intermediate outputs
+        self.layers = [self.conv1, self.layer1, self.layer2, self.layer3, self.layer4, self.classifier]
 
     def _make_layer(self, block, planes, num_blocks, stride):
         # eg: [2, 1, 1, ..., 1]. Only the first one downsamples.
@@ -62,21 +79,24 @@ class PreActResNet(nn.Module):
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
-    # Modified to enable stitching!
-    def forward(self, x, out_layer=None):
-        assert 0 < out_layer and out_layer < 5
+    # Modified to enable stitching; in_layer is the layer that we are inputting into
+    # while outfrom layer is the layer we are pulling the output from. We assume 5 layers total.
+    def forward(self, x, into_layer=0, outfrom_layer=5):
+        assert into_layer <= outfrom_layer
+        assert 0 <= into_layer and into_layer < len(self.layers)
+        assert 0 <= outfrom_layer and outfrom_layer < len(self.layers)
 
-        out = self.conv1(x)
-        # Enable intermediate outputs, might be a little slower
-        for idx, layer in enumerate([self.layer1, self.layer2, self.layer3, self.layer4]):
+        out = x
+        for idx in range(into_layer, outfrom_layer+1):
+            layer = self.layers[idx]
             out = layer(out)
-            if out_layer == idx + 1:
-                return out
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
         return out
 
+# def resnet_stitch(in_shape, out_shape):
+#     pass
+
+# def stitched_resnet():
+#     pass
 
 def resnet18k_cifar(k = 64, num_classes=10) -> PreActResNet: # k = 64 is the standard ResNet18
     return PreActResNet(PreActBlock, [2, 2, 2, 2], num_classes=num_classes, init_channels=k)
