7638caae58930f80709b024f168a36bafa6e905b
Adriano Hernandez <adrianoh@mit.edul>
2022-04-13 23:34:36 -0400

    wip

--- rumen/stitching/layer_label.py
+++ rumen/stitching/layer_label.py
@@ -9,6 +9,7 @@ from typing_extensions import (
     ParamSpec,
 )
 from typing import (
+    Dict,
     NoReturn,
     Callable,
     Union,
@@ -17,6 +18,8 @@ from typing import (
     Tuple,
     TypeVar,
 )
+
+from sympy import assemble_partfrac_list
 # https://docs.python.org/3/library/typing.html#typing.ParamSpec
 T = TypeVar('T')
 P = ParamSpec('P')
@@ -44,7 +47,7 @@ class LayerLabel(object):
     CONV1: str = "conv1"
     FC: str = "fc"
 
-    """ Documentation, basically """
+    """ 4 blocksets indexed by 1, while blocks are indexed by 0 """
     BLOCKSET_MIN: int = 1
     BLOCK_MIN: int = 0
     BLOCKSET_MAX: int = 4
@@ -104,6 +107,30 @@ class LayerLabel(object):
 
             return func(self, *args, **kwargs)
         return wrapper
+    ################################################################################
+
+    @checkValid
+    def __repr__(self: LayerLabel) -> str:
+        if self.str_label is None:
+            return str((self.blockset, self.block))
+        else:
+            return self.str_label
+
+    @checkValid
+    def __str__(self: LayerLabel) -> str:
+        return repr(self)
+
+    @checkValid
+    def __eq__(self: LayerLabel, other: LayerLabel) -> bool:
+        return type(other) == LayerLabel and \
+            self.str_label == other.str_label and \
+            self.blockset == other.blockset and self.block == other.block and \
+            self.model_blocksets == other.model_blocksets
+
+    @checkValid
+    def __hash__(self) -> int:
+        return repr(self)
+    ################################################################################
 
     @checkValid
     def isInput(self: LayerLabel) -> bool:
@@ -112,9 +139,11 @@ class LayerLabel(object):
     @checkValid
     def isOutput(self: LayerLabel) -> bool:
         return self.str_label == LayerLabel.OUTPUT
+    ################################################################################
 
     @checkValid
     def prevLabel(self: LayerLabel) -> LayerLabel:
+        # Get the label that would represent the layer before the one repped by this label
         model_blocksets = self.model_blocksets
         blockset = self.blockset
         block = self.block
@@ -151,38 +180,17 @@ class LayerLabel(object):
                 f"Not Supported: prevLabel({str_label} | ({blockset}, {block}))")
 
     @checkValid
-    def __repr__(self: LayerLabel) -> str:
-        if self.str_label is None:
-            return str((self.blockset, self.block))
-        else:
-            return self.str_label
-
-    @checkValid
-    def __str__(self: LayerLabel) -> str:
-        return repr(self)
-
-    @checkValid
-    def __eq__(self: LayerLabel, other: LayerLabel) -> bool:
-        return type(other) == LayerLabel and \
-            self.str_label == other.str_label and \
-            self.blockset == other.blockset and self.block == other.block and \
-            self.model_blocksets == other.model_blocksets
-
-    @checkValid
-    def __hash__(self) -> int:
-        return repr(self)
-
-    @checkValid
     def __sub__(self: LayerLabel, other: int) -> LayerLabel:
-        assert type(other) == int, "other must be an int"
-        assert other >= 0, "other must be non-negative"
+        assert type(other) == int, "other must be an int in __sub__"
+        assert other >= 0, "other must be non-negative in __sub__"
         label: LayerLabel = self
         for _ in range(other):
             label = label.prevLabel()
         return label
+    ################################################################################
 
     @checkValid
-    def layerIndex(self: LayerLabel) -> int:
+    def label2idx(self: LayerLabel) -> int:
         # Return the index of the layer in the model assuming that
         # the model is input -> conv1 -> blocksets -> fc -> output
         # 0-indexing by conv1. NOTE that blocks are treated as single
@@ -192,12 +200,12 @@ class LayerLabel(object):
         elif self.str_label == LayerLabel.CONV1:
             return 0
         elif self.str_label is None:
-            blockset = self.blockset
-            block = self.block
-            assert not blockset is None, "blockset cannot be None if str_label is None (layerIndex)"
-            assert not block is None, "block cannot be None if str_label is None (layerIndex)"
+            blockset: int = self.blockset
+            block: int = self.block
+            assert not blockset is None, "blockset cannot be None if str_label is None (label2idx)"
+            assert not block is None, "block cannot be None if str_label is None (label2idx)"
 
-            block_idx = block - LayerLabel.BLOCK_MIN
+            block_idx: int = block - LayerLabel.BLOCK_MIN
             blockset_idx: int = blockset - LayerLabel.BLOCKSET_MIN
             prefix: List[int] = self.model_blocksets[:blockset_idx]
             prefix_num_blocks: int = sum(prefix)
@@ -206,18 +214,96 @@ class LayerLabel(object):
 
         elif self.str_label == LayerLabel.FC:
             # conv1 + blocksets (number of blocks per blockset in the list) + fc
-            num_layers = 2 + sum(self.model_blocksets)
+            num_layers: int = 2 + sum(self.model_blocksets)
             return num_layers - 1
         else:
             raise Exception(
-                f"Not Supported: layerIndex({self.str_label} | ({self.blockset}, {self.block}))")
+                f"Not Supported: label2idx({self.str_label} | ({self.blockset}, {self.block}))")
+    ################################################################################
+
+    @staticmethod
+    def numLayers(R: List[int]) -> int:
+        return 2 + sum(R)
+
+    @staticmethod
+    def idx2label(idx: int, R: List[int]) -> LayerLabel:
+        # Given an index, return the label that represents it
+        if idx == 0:
+            return LayerLabel(LayerLabel.CONV1, R)
+        else:
+            num_layers: int = LayerLabel.numLayers(R)
+            fc_idx: int = num_layers - 1
+            if idx == fc_idx:
+                return LayerLabel(LayerLabel.FC, R)
+            elif idx < fc_idx:
+                remaining_idx: int = idx - 1
+                blockset_idx = 0
+                while remaining_idx >= R[blockset_idx]:
+                    remaining_idx -= R[blockset_idx]
+                    blockset_idx += 1
+
+                blockset: int = blockset_idx + LayerLabel.BLOCKSET_MIN
+                block: int = remaining_idx + LayerLabel.BLOCK_MIN
+                return LayerLabel((blockset, block), R)
+            else:
+                raise Exception(
+                    f"idx {idx} is not a valid index in LayerLabel.idx2label")
+
+    @staticmethod
+    def labels(R: List[int]) -> List[LayerLabel]:
+        # Get a list of labels for a given ResNet
+        l = []
+        l.append(LayerLabel(LayerLabel.CONV1, R))
+        for blockset, num_blocks in enumerate(R, LayerLabel.BLOCKSET_MIN):
+            for block in range(LayerLabel.BLOCK_MIN, LayerLabel.BLOCK_MIN + num_blocks):
+                l.append(LayerLabel((blockset, block), R))
+        l.append(LayerLabel(LayerLabel.FC, R))
+        return l
+
+    @staticmethod
+    def generateTable(constructor: Callable[[LayerLabel, LayerLabel], T], R1: List[int], R2: List[int]) -> Tuple[List[List[T]], Dict[(int, int), Tuple[LayerLabel, LayerLabel]]]:
+        # Generate a table of objects of a given type for two networks. The use cases are:
+        # - stitches: the constructor is a stitch constructor
+        # - stitched networks: the constructor is a stitched network constructor
+        # - similarities: the constructor is a training function on a pre-existing table/networks
+        # - mean2 differences: the constructor is a mean2 difference function on a pre-existing table/networks
+        # NOTE: we stitch from all layers except FC, but we stitch INTO all layers
+        num_layers1: int = LayerLabel.numLayers(R1)
+        num_layers2: int = LayerLabel.numLayers(R2)
+
+        height = num_layers1 - 1
+        width = num_layers2
+
+        # all of R1, except FC sends INTO all of R2
+        labels1: List[LayerLabel] = LayerLabel.labels(R1)
+        labels1.pop()
+
+        labels2: List[LayerLabel] = LayerLabel.labels(R2)
+        idx2label = {
+            (i, j): (labels1[i], labels2[j]) for i in range(height) for j in range(width)
+        }
+        return [[constructor(*idx2label[(i, j)]) for j in range(width)] for i in range(height)], idx2label
 
 
 class TestLayerLabel(unittest.TestCase):
-    R2222 = [2, 2, 2, 2]
-    R1111 = [1, 1, 1, 1]
+    """
+    Extensive unit testing for LayerLabel. This mainly checks that
+    - checkValid is being used to confirm the correctness of the representation properly
+    - equality works (which should imply that the representation, hashing, etc... are OK)
+    - you can get previous labels given labels (used to know which layers are compared when
+      you have the label of the sender layer and the label of the reciever layer)
+    - conversion to and from layers and indices works
+    - you are able to get an array that maps indices to labels for a single network (i.e. it
+      orders the layers by index)
+    - you can generate tables (just doubly nested Arrays), given a closure, to do things like,
+      for example, create stitch tables (and this also returns a mapping from index tuples to
+      label tuples representing the sending and recieving layers in their corresponding networks)
+    """
+    R2222: List[int] = [2, 2, 2, 2]
+    R1111: List[int] = [1, 1, 1, 1]
+    R1234: List[int] = [1, 2, 3, 4]
 
-    def test_checkValid(self):
+    def test_checkValid(self: TestLayerLabel) -> NoReturn:
         """ Check that an exception is thrown when the representation of this label is invalid """
         invalidStr: str = "invalidString"
         invalidTuple2: Tuple[int, int] = (-1, -1)
@@ -230,56 +316,140 @@ class TestLayerLabel(unittest.TestCase):
         self.assertRaises(Exception, input2.prevLabel)
         self.assertRaises(Exception, input3.prevLabel)
 
-    def test_equals(self):
+    def test_equals(self: TestLayerLabel) -> NoReturn:
         """ Check that equality works """
-        b1: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R1111)
-        b2: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R1111)
-        s1: LayerLabel = LayerLabel("conv1", TestLayerLabel.R1111)
-        s2: LayerLabel = LayerLabel("conv1", TestLayerLabel.R1111)
+        b1: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R2222)
+        b2: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R2222)
+        b3: LayerLabel = LayerLabel((2, 0), TestLayerLabel.R2222)
+        b4: LayerLabel = LayerLabel((1, 1), TestLayerLabel.R2222)
+        s1: LayerLabel = LayerLabel(LayerLabel.CONV1, TestLayerLabel.R2222)
+        s2: LayerLabel = LayerLabel(LayerLabel.CONV1, TestLayerLabel.R2222)
+        s3: LayerLabel = LayerLabel(LayerLabel.FC, TestLayerLabel.R2222)
+        s4: LayerLabel = LayerLabel(LayerLabel.CONV1, TestLayerLabel.R1234)
 
+        # Make sure that identical objects (underlying: tuple) are equal
         self.assertTrue(b1 == b1)
         self.assertTrue(b1 == b2)
         self.assertTrue(b2 == b1)
 
+        # Make sure that strings and non-identical objects are not equal
         self.assertFalse(b1 == s1)
         self.assertFalse(s1 == b1)
         self.assertFalse(b1 == s2)
 
+        # Make sure that identical objects (underlying: string) are equal
         self.assertTrue(s1 == s1)
         self.assertTrue(s1 == s2)
         self.assertTrue(s2 == s1)
 
-    def test_prevLabel(self):
+        # Make sure that if you vary the blockset, you get a different object
+        self.assertFalse(s1 == s4)
+
+        # Make sure that if you vary the tuple you get a different object
+        self.assertFalse(b1 == b3)
+        self.assertFalse(b1 == b4)
+
+        # Make sure that if you vary the string you get a different object
+        self.assertFalse(s1 == s3)
+
+    def test_prevLabel(self: TestLayerLabel) -> NoReturn:
         """  Test that lbl.prevLabel() and shorthand `lbl - 1` works with cached model blocksets"""
-        input: LayerLabel = LayerLabel((1, 1), TestLayerLabel.R2222)
-        expected1: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R2222)
-        expected2: LayerLabel = LayerLabel("conv1", TestLayerLabel.R2222)
-
-        self.assertEqual(input.prevLabel(), expected1)
-        self.assertEqual(input - 1, expected1)
-        self.assertEqual(input.prevLabel().prevLabel(), expected2)
-        self.assertEqual(input - 2, expected2)
-        self.assertEqual(input - 1 - 1, expected2)
-
-    def test_layerIndex(self):
-        """ Test that lbl.layerIndex() works properly """
-        # [0: conv1, 1: (1, 0), 2: (1, 1), ...]
         input1: LayerLabel = LayerLabel((1, 1), TestLayerLabel.R2222)
-        expected1: int = 2
+        expected1: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R2222)
+        expected2: LayerLabel = LayerLabel(
+            LayerLabel.CONV1, TestLayerLabel.R2222)
+
+        input2: LayerLabel = LayerLabel((3, 0), TestLayerLabel.R1234)
+        expected3: LayerLabel = LayerLabel((2, 1), TestLayerLabel.R1234)
+        expected4: LayerLabel = LayerLabel((2, 0), TestLayerLabel.R1234)
+        expected5: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R1234)
+        expected6: LayerLabel = LayerLabel(
+            LayerLabel.CONV1, TestLayerLabel.R1234)
+
+        # Check that the prevLabel() method works and that __sub__ calls it properly
+        self.assertEqual(input1.prevLabel(), expected1)
+        self.assertEqual(input1 - 1, expected1)
+        self.assertEqual(input1.prevLabel().prevLabel(), expected2)
+        self.assertEqual(input1 - 2, expected2)
+        self.assertEqual(input1 - 1 - 1, expected2)
+
+        # Check that when size is changing, it still works
+        self.assertEqual(input2 - 1, expected3)
+        self.assertEqual(input2 - 2, expected4)
+        self.assertEqual(input2 - 3, expected5)
+        self.assertEqual(input2 - 4, expected6)
+
+    def test_label2idx_and_idx2layer(self: TestLayerLabel) -> NoReturn:
+        """ Test that lbl.label2idx() works properly """
+        # [0: conv1, 1: (1, 0), 2: (1, 1), ...]
+        label1: LayerLabel = LayerLabel((1, 1), TestLayerLabel.R2222)
+        idx1: int = 2
 
         # [0: conv1, 1: (1, 0), 2: (2, 0), ...]
-        input2 = LayerLabel(LayerLabel.CONV1, TestLayerLabel.R1111)
-        input3: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R1111)
-        input4 = LayerLabel((2, 0), TestLayerLabel.R1111)
+        label2: LayerLabel = LayerLabel(LayerLabel.CONV1, TestLayerLabel.R1111)
+        label3: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R1111)
+        label4: LayerLabel = LayerLabel((2, 0), TestLayerLabel.R1111)
+        idx2: int = 0
+        idx3: int = 1
+        idx4: int = 2
 
-        expected2: int = 0
-        expected3: int = 1
-        expected4: int = 2
+        label5: LayerLabel = LayerLabel(LayerLabel.CONV1, TestLayerLabel.R1234)
+        label6: LayerLabel = LayerLabel((1, 0), TestLayerLabel.R1234)
+        label7: LayerLabel = LayerLabel((2, 0), TestLayerLabel.R1234)
+        label8: LayerLabel = LayerLabel((2, 1), TestLayerLabel.R1234)
+        label9: LayerLabel = LayerLabel((3, 0), TestLayerLabel.R1234)
+        label10: LayerLabel = LayerLabel((3, 1), TestLayerLabel.R1234)
+        label11: LayerLabel = LayerLabel((3, 2), TestLayerLabel.R1234)
+        idx5: int = 0
+        idx6: int = 1
+        idx7: int = 2
+        idx8: int = 3
+        idx9: int = 4
+        idx10: int = 5
+        idx11: int = 6
 
-        self.assertEqual(input1.layerIndex(), expected1)
-        self.assertEqual(input2.layerIndex(), expected2)
-        self.assertEqual(input3.layerIndex(), expected3)
-        self.assertEqual(input4.layerIndex(), expected4)
+        # Check that label2idx() works
+        self.assertEqual(label1.label2idx(), idx1)
+        self.assertEqual(label2.label2idx(), idx2)
+        self.assertEqual(label3.label2idx(), idx3)
+        self.assertEqual(label4.label2idx(), idx4)
+        self.assertEqual(label5.label2idx(), idx5)
+        self.assertEqual(label6.label2idx(), idx6)
+        self.assertEqual(label7.label2idx(), idx7)
+        self.assertEqual(label8.label2idx(), idx8)
+        self.assertEqual(label9.label2idx(), idx9)
+        self.assertEqual(label10.label2idx(), idx10)
+        self.assertEqual(label11.label2idx(), idx11)
+
+        # Check that idx2label() works
+        self.assertEqual(LayerLabel.idx2label(
+            idx1, TestLayerLabel.R2222), label1)
+        self.assertEqual(LayerLabel.idx2label(
+            idx2, TestLayerLabel.R1111), label2)
+        self.assertEqual(LayerLabel.idx2label(
+            idx3, TestLayerLabel.R1111), label3)
+        self.assertEqual(LayerLabel.idx2label(
+            idx4, TestLayerLabel.R1111), label4)
+        self.assertEqual(LayerLabel.idx2label(
+            idx5, TestLayerLabel.R1234), label5)
+        self.assertEqual(LayerLabel.idx2label(
+            idx6, TestLayerLabel.R1234), label6)
+        self.assertEqual(LayerLabel.idx2label(
+            idx7, TestLayerLabel.R1234), label7)
+        self.assertEqual(LayerLabel.idx2label(
+            idx8, TestLayerLabel.R1234), label8)
+        self.assertEqual(LayerLabel.idx2label(
+            idx9, TestLayerLabel.R1234), label9)
+        self.assertEqual(LayerLabel.idx2label(
+            idx10, TestLayerLabel.R1234), label10)
+        self.assertEqual(LayerLabel.idx2label(
+            idx11, TestLayerLabel.R1234), label11)
+
+    def test_labels(self: TestLayerLabel) -> NoReturn:
+        raise NotImplementedError  # TODO
+
+    def test_generateTable(self: TestLayerLabel) -> NoReturn:
+        raise NotImplementedError  # TODO
 
 
 if __name__ == '__main__':
